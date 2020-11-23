00e1934b07bccf8a5b30b6cd5212ba175fe86a6d
chigriil <dachig8@gmail.com>
2020-11-23 21:46:35 +0500

    Сompletely new version of the game for the second part of the task.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pygame as pg
 from random import randint
-import time
 
 pg.init()
 pg.font.init()
+all_sprites = pg.sprite.Group()
 
 # The description of colors
 
@@ -16,18 +16,17 @@ SCREEN_SIZE = (800, 600)
 
 
 # This function randomly generates color
-
 def rand_color():
     return randint(0, 255), randint(0, 255), randint(0, 255)
 
 
 class Ball:
     """
-    The ball class. Creates a ball, controls it's movement and implement it's
-    rendering.
+    The ball class. Creates a two types of ball, controls it's movement and
+    implement it's rendering.
     """
 
-    def __init__(self, coord, vel, rad=20, color=None):
+    def __init__(self, coord, vel, rad=20, color=None, width=None):
         """
         Constructor method. Initializes ball's parameters and initial values.
         """
@@ -37,6 +36,9 @@ class Ball:
             color = rand_color()
         self.color = color
         self.rad = rad
+        if width is None:
+            width = randint(0, 1)
+        self.width = width
         self.is_alive = True
 
     def check_corners(self, refl_ort=0.8, refl_par=0.9):
@@ -54,7 +56,7 @@ class Ball:
                 self.vel[i] = -int(self.vel[i] * refl_ort)
                 self.vel[1 - i] = int(self.vel[1 - i] * refl_par)
 
-    def move(self, time=1, grav=0):
+    def move(self, time=1, grav=3):
         """
         Moves the ball according to it's velocity and time step.
         Changes the ball's velocity due to gravitational force.
@@ -71,7 +73,7 @@ class Ball:
         """
         Draws the ball on appropriate surface.
         """
-        pg.draw.circle(screen, self.color, self.coord, self.rad)
+        pg.draw.circle(screen, self.color, self.coord, self.rad, self.width)
 
 
 class Target:
@@ -80,7 +82,7 @@ class Target:
     ball event.
     """
 
-    def __init__(self, coord=None, color=None, rad=30):
+    def __init__(self, coord=None, color=None, rad=30, width=0):
         """
         Constructor method. Sets coordinate, color and radius of the target.
         """
@@ -89,6 +91,7 @@ class Target:
                      randint(rad, SCREEN_SIZE[1] - rad)]
         self.coord = coord
         self.rad = rad
+        self.width = width
 
         if color is None:
             color = rand_color()
@@ -107,7 +110,7 @@ class Target:
         """
         Draws the target on the screen
         """
-        pg.draw.circle(screen, self.color, self.coord, self.rad)
+        pg.draw.circle(screen, self.color, self.coord, self.rad, self.width)
 
     def move(self):
 
@@ -118,7 +121,8 @@ class MovingTarget(Target):
     """
     moving target class, manges moving of a target
     """
-    def __init__(self, coord=None, color=None, rad=30):
+
+    def __init__(self, coord=None, color=None, rad=30, width=0):
         """
         Constructor method. Sets coordinate, color, radius
          Ox and Oy speed of the target.
@@ -129,6 +133,8 @@ class MovingTarget(Target):
         super().__init__(coord, color, rad)
         self.vx = randint(-2, +2)
         self.vy = randint(-2, +2)
+        if width == 0:
+            self.width = 5
 
     def wall_collision(self):
         """
@@ -150,80 +156,75 @@ class MovingTarget(Target):
         self.coord[1] += self.vy
 
 
+class TankSprite(pg.sprite.Sprite):
+    """
+    This class maneges work of a tank sprite
+    """
+    def __init__(self, team_color):
+        pg.sprite.Sprite.__init__(self)
+        self.color = team_color
+        if self.color == RED:
+            self.image = Tpic
+        elif self.color == WHITE:
+            self.image = Tpic1
+        self.rect = self.image.get_rect()
+        self.rect.center = (30, 30)
+
+    def update(self, xcor, ycor):
+        self.rect.x = xcor - 70
+        self.rect.y = ycor - 30
+
+
 class Gun:
     """
     Gun class. Manages it's renderring, movement and striking.
     """
-
-    def __init__(self, coord=[30, SCREEN_SIZE[1] // 2], angle=0, max_pow=70,
-                 min_pow=10, color=RED):
-        """
-        Constructor method. Sets coordinate, direction, minimum and maximum
-        power and color of the gun.
-        """
-        self.coord = coord
-        self.angle = angle
-        self.max_pow = max_pow
+    def __init__(self, team_color,
+                 min_pow=20, max_pow=50):
+        self.color = team_color
+        self.coord = [30, SCREEN_SIZE[1] // 2]
+        if self.color == WHITE:
+            self.coord[0] = 770
+        self.angle = 0
         self.min_pow = min_pow
-        self.color = color
+        self.max_pow = max_pow
+        self.power = min_pow
         self.active = False
-        self.pow = min_pow
+        self.alive = True
+        self.sprite = TankSprite(self.color)
+        self.radius = 30
 
-    def activate(self):
-        """
-        Activates gun's charge.
-        """
-        self.active = True
-
-    def gain(self, inc=2):
-        """
-        Increases current gun charge power.
-        """
-        if self.active and self.pow < self.max_pow:
-            self.pow += inc
-
-    def strike(self):
-        """
-        Creates ball, according to gun's direction and current charge power.
-        """
-        vel = self.pow
-        angle = self.angle
-        ball = Ball(list(self.coord),
-                    [int(vel * np.cos(angle)), int(vel * np.sin(angle))])
-        self.pow = self.min_pow
-        self.active = False
-        return ball
-
-    def set_angle(self, target_pos):
-        """
-        Sets gun's direction to target position.
-        """
-        self.angle = np.arctan2(target_pos[1] - self.coord[1],
-                                target_pos[0] - self.coord[0])
-
-    def move(self, inc):
-        """
-        Changes vertical position of the gun.
-        """
-        if (self.coord[1] > 30 or inc > 0) and (
-                self.coord[1] < SCREEN_SIZE[1] - 30 or inc < 0):
-            self.coord[1] += inc
+    def hit_target(self, hit_coord):
+        if ((hit_coord[0] - self.coord[0]) ** 2 + (hit_coord[1] -
+                                                   self.coord[1]) ** 2
+                <= (self.radius + 15) ** 2):
+            self.coord = [30, SCREEN_SIZE[1] // 2]
+            if self.color == WHITE:
+                self.coord[0] = 770
+            return 1
+        else:
+            return 0
 
     def draw(self, screen):
-        """
-        Draws the gun on the screen.
-        """
-        gun_shape = []
-        vec_1 = np.array([int(5 * np.cos(self.angle - np.pi / 2)),
-                          int(5 * np.sin(self.angle - np.pi / 2))])
-        vec_2 = np.array([int(self.pow * np.cos(self.angle)),
-                          int(self.pow * np.sin(self.angle))])
-        gun_pos = np.array(self.coord)
-        gun_shape.append((gun_pos + vec_1).tolist())
-        gun_shape.append((gun_pos + vec_1 + vec_2).tolist())
-        gun_shape.append((gun_pos + vec_2 - vec_1).tolist())
-        gun_shape.append((gun_pos - vec_1).tolist())
-        pg.draw.polygon(screen, self.color, gun_shape)
+        end_pos = [self.coord[0] + self.power * np.cos(self.angle),
+                   self.coord[1] + self.power * np.sin(self.angle)]
+        pg.draw.line(screen, self.color, self.coord, end_pos, 5)
+        self.sprite.update(self.coord[0], self.coord[1])
+
+    def strike(self):
+        vel = [int(self.power * np.cos(self.angle)),
+               int(self.power * np.sin(self.angle))]
+        self.active = False
+        self.power = self.min_pow
+        return Ball(list(self.coord), vel)
+
+    def move(self):
+        if self.active and self.power < self.max_pow:
+            self.power += 1
+
+    def set_angle(self, mouse_pos):
+        self.angle = np.arctan2(mouse_pos[1] - self.coord[1],
+                                mouse_pos[0] - self.coord[0])
 
 
 class ScoreTable:
@@ -259,7 +260,7 @@ class Manager:
 
     def __init__(self, n_targets=1):
         self.balls = []
-        self.gun = Gun()
+        self.guns = [Gun(RED), Gun(WHITE)]
         self.targets = []
         self.score_t = ScoreTable()
         self.n_targets = n_targets
@@ -284,11 +285,6 @@ class Manager:
         previous are destroyed.
         """
         done = self.handle_events(events)
-
-        if pg.mouse.get_focused():
-            mouse_pos = pg.mouse.get_pos()
-            self.gun.set_angle(mouse_pos)
-
         self.move()
         self.collide()
         self.draw(screen)
@@ -308,16 +304,38 @@ class Manager:
                 done = True
             elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_UP:
-                    self.gun.move(-5)
+                    self.guns[1].coord[1] -= 20
                 elif event.key == pg.K_DOWN:
-                    self.gun.move(5)
+                    self.guns[1].coord[1] += 20
+                elif event.key == pg.K_LEFT:
+                    self.guns[1].coord[0] -= 20
+                elif event.key == pg.K_RIGHT:
+                    self.guns[1].coord[0] += 20
+                if event.key == pg.K_w:
+                    self.guns[0].coord[1] -= 20
+                elif event.key == pg.K_s:
+                    self.guns[0].coord[1] += 20
+                elif event.key == pg.K_a:
+                    self.guns[0].coord[0] -= 20
+                elif event.key == pg.K_d:
+                    self.guns[0].coord[0] += 20
             elif event.type == pg.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    self.gun.activate()
+                    self.guns[0].active = True
+                if event.button == 3:
+                    self.guns[1].active = True
             elif event.type == pg.MOUSEBUTTONUP:
                 if event.button == 1:
-                    self.balls.append(self.gun.strike())
+                    self.balls.append(self.guns[0].strike())
                     self.score_t.b_used += 1
+                if event.button == 3:
+                    self.balls.append(self.guns[1].strike())
+                    self.score_t.b_used += 1
+        if pg.mouse.get_focused():
+            mouse_pos = pg.mouse.get_pos()
+            for gun in self.guns:
+                gun.set_angle(mouse_pos)
+
         return done
 
     def draw(self, screen):
@@ -328,7 +346,9 @@ class Manager:
             ball.draw(screen)
         for target in self.targets:
             target.draw(screen)
-        self.gun.draw(screen)
+        all_sprites.draw(screen)
+        for gun in self.guns:
+            gun.draw(screen)
         self.score_t.draw(screen)
 
     def move(self):
@@ -337,14 +357,15 @@ class Manager:
         """
         dead_balls = []
         for i, ball in enumerate(self.balls):
-            ball.move(grav=2)
+            ball.move()
             if not ball.is_alive:
                 dead_balls.append(i)
         for i in reversed(dead_balls):
             self.balls.pop(i)
         for i, target in enumerate(self.targets):
             target.move()
-        self.gun.gain()
+        for gun in self.guns:
+            gun.move()
 
     def collide(self):
         """
@@ -365,11 +386,14 @@ class Manager:
 
 screen = pg.display.set_mode(SCREEN_SIZE)
 pg.display.set_caption("Пушка")
+Tpic = pg.image.load('T90pic.png').convert()
+Tpic1 = pg.image.load('T90pic1.png').convert()
 
 done = False
 clock = pg.time.Clock()
 
 mgr = Manager(n_targets=3)
+all_sprites.add(mgr.guns[0].sprite, mgr.guns[1].sprite)
 
 while not done:
     clock.tick(15)
